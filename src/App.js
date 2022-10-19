73bb950f289f048329ea374ebeded7868d59414d
jgjs <rkfwltn12@naver.com>
2022-10-19 15:42:01 +0900

    react js inflearn강의

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MyHeader from "./MyHeader";
+import Counter from "./Counter";
+import Container from "./Container";
+
 function App() {
+    const counterProps = {
+        a:1,
+        b:2,
+        c:3,
+        d:4,
+        initialValue: 5,
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <div className="App">
+          <MyHeader />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+            <Counter {...counterProps} />
+        </div>
+      </Container>
   );
 }
 
---
+++ src/MyHeader.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const style = {
+    t: {
+        color: "yellow"
+    },
+    e: {
+        color: "orange"
+    }
+}
+
+const MyHeader = () => {
+    return <header className="" style={style.t}>헤더</header>;
+}
+
+export default MyHeader;
